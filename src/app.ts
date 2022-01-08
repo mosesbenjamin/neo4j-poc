9d9040268efc412fbb61c1ade4de5a12c61f2689
mosesbenjamin <mavewreek@gmail.com>
2022-01-08 22:43:18 +0100

    add updates

--- src/app.ts
+++ src/app.ts
@@ -17,18 +17,105 @@ app.use(express.static(path.join(__dirname, 'public')));
 const driver: typeof neo4j.Driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'test'))
 const session: typeof  neo4j.Session = driver.session();
 
+interface CyperQuery {
+    statement: string,
+    params?: object
+}
+
+interface Repos {
+    id: number,
+    name: string
+}
+
+interface Images {
+    id: number,
+    digest: string,
+    osarch: string,
+    compressedsizemb: number
+}
+
+const executeCypherQuery = async (options: CyperQuery) => {
+    try {
+      const result = await session.run(options.statement, options.params || {});
+      return result
+    } catch (error) {
+      throw error;
+    }
+}
 
 
-app.get('/', (req, res) => {
-    session
-    .run('MATCH(n) RETURN (n)')
-    .then((res) => {
-        res.records.forEach((record: any) => {
-            console.log(record._fields[0].properties)
+app.get('/', async (_, res) => {
+    const repoResult = await executeCypherQuery({ 
+        statement: `MATCH(n:ContainerRepository) RETURN (n)`
+     })
+    const imageResult = await executeCypherQuery({
+        statement: `MATCH(n:ContainerImage) RETURN (n)`
+    })
+
+    let repos: Repos[] = [];
+    repoResult.records.forEach((record: any ) => {
+        repos.push({
+            id: record._fields[0].identity.low,
+            name: record._fields[0].properties.name,
+        })
+    }) 
+
+    let images: Images[] = [];
+    imageResult.records.forEach((record: any ) => {
+        images.push({
+            id: record._fields[0].identity.low,
+            digest: record._fields[0].properties.digest,
+            osarch: record._fields[0].properties.osarch,
+            compressedsizemb: record._fields[0].properties.compressedsizemb
         })
     })
-    .catch((err) => console.error(err))
-    res.send('Holla!')
+
+    res.render('index', { repos, images });
+})
+
+app.post('/image/add', async (req, res) => {
+    const digest = req.body.digest
+    const osarch = req.body.osarch
+    const compressedsizemb = req.body.compressedsizemb
+
+    const params = {
+       digest,
+       osarch,
+       compressedsizemb
+    };
+
+     await executeCypherQuery({
+        statement: `CREATE(n:ContainerImage {digest:$digest, osarch:$osarch, compressedsizemb:$compressedsizemb}) RETURN n`,
+        params
+    })
+    res.redirect("/")
+})
+
+app.post('/repo/add', async (req, res) => {
+    const name = req.body.name
+
+    const params = { name };
+
+     await executeCypherQuery({
+        statement: `CREATE (n:ContainerRepository{name:$name})`,
+        params
+    })
+    res.redirect("/")
+})
+
+app.post('/repo/image/add', async (req, res) => {
+    const image = req.body.image
+    const repo = req.body.repo
+
+    const params = { image, repo }
+
+    await executeCypherQuery({
+        statement: `MATCH (image:ContainerImage), (repo:ContainerRepository)
+        WHERE repo.name = $repo AND image.digest = $image
+        CREATE (repo)-[r:TAG]->(image);`,
+        params
+    })
+    res.redirect("/")
 })
 
 const PORT = 9800
